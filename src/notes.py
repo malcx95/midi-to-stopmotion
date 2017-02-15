1e4676ae74ec1cafd429df0fd4a81a7f6a5cd49c
Malcolm Wigren <trekommafem2@gmail.com>
2017-02-15 08:53:12 +0100

    Implemented get_octave and get_tone

--- src/notes.py
+++ src/notes.py
@@ -1,4 +1,5 @@
 
+
 TONES = ['C', 'Cs', 'D', 'Ds', 'E', 'F', 'Fs', 'G', 'Gs', 'A', 'As', 'B']
 
 class Note:
@@ -7,14 +8,11 @@ class Note:
     def __init__(self, note_number):
         self.note_number = note_number
         self.octave = _get_octave(note_number)
-        self.tone = _get_tone(note_number)
+        self.tone = _get_tone(note_number, self.octave)
 
 
 def _get_octave(note_number):
-    # TODO implement
-    return 0
+    return note_number // len(TONES)
 
-def _get_tone(note_number):
-    # TODO implement
-    return TONES[0]
-
+def _get_tone(note_number, octave):
+    return TONES[note_number - octave * len(TONES)]
